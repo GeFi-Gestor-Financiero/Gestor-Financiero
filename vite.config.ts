719ad8cc0314d2ec381fb875e2481643decb5d70
Lautaro Denis <denislautaro6@gmail.com>
2026-07-03 20:07:03 -0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Gestor-Financiero/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
