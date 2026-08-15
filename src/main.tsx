082c467215368d2a892ec581250e4ec6da9af26c
Lautaro Denis <denislautaro6@gmail.com>
2026-08-14 21:01:54 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
-import { NotificationProvider } from './context/NotificationContext.tsx'
+import { NotificationProvider } from './context/NotificationContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
